82572781774ef344d6590e35d1e9048d10a299c9
Doreen Hakimi <doreen.hakimi@gmail.com>
2017-07-19 22:47:58 -0700

    Squashed commit of the following:
    commit d36ce74f6738a482833825426ad0faace8a84f47
    Author: Doreen Hakimi <doreen.hakimi@gmail.com>
    Date:   Wed Jul 19 22:47:09 2017 -0700
    
        dude side nav finally done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ClassNames from 'classnames';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {ListItem} from 'material-ui/List';
 import logo from './logo.svg';
 import './App.css';
 
@@ -12,46 +14,52 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className={ClassNames({'App-side-nav': true, expanded: this.state.sideNavExpanded})}>
-          <div className={ClassNames({'side-nav-header': true, hidden: !this.state.sideNavExpanded})}>
-            <img src={logo} className="App-logo" alt="logo" />
-            <div className={ClassNames({title: true, hidden: !this.state.sideNavExpanded})}>Mentalist</div>
+      <MuiThemeProvider>
+        <div className="App">
+          <div className={ClassNames({'App-side-nav': true, expanded: this.state.sideNavExpanded})}>
+            <div className={ClassNames({'side-nav-header': true, expanded: this.state.sideNavExpanded})}>
+              <ListItem className="nav-item">
+                <div className='label'>Daily</div>
+                <div className="App-logo nav-logo">
+                  <img src={logo} className="App-logo" alt="logo" />
+                </div>
+              </ListItem>
+            </div>
+            <div className={ClassNames({'side-nav-contents': true, expanded: this.state.sideNavExpanded})}>
+              <ListItem className="nav-item">
+                <div className='label'>Daily</div>
+                <div className="App-logo nav-logo">
+                  <img src={logo} className="App-logo" alt="logo" />
+                </div>
+              </ListItem>
+              <ListItem className="nav-item">
+                <div className='label'>Daily</div>
+                <div className="App-logo nav-logo">
+                  <img src={logo} className="App-logo" alt="logo" />
+                </div>
+              </ListItem>
+              <ListItem className="nav-item">
+                <div className='label'>Daily</div>
+                <div className="App-logo nav-logo">
+                  <img src={logo} className="App-logo" alt="logo" />
+                </div>
+              </ListItem>
+            </div>
+            <div className="toggle-collapse" onClick={() => this.setState({sideNavExpanded: !this.state.sideNavExpanded})}>
+              {this.state.sideNavExpanded &&  'c'}
+              {!this.state.sideNavExpanded &&  'e'}
+            </div>
           </div>
-          <div className={ClassNames({'side-nav-contents': true, expanded: this.state.sideNavExpanded})}>
-            <div className="nav-item">
-              <div className='label'>Daily</div>
-              <div className="App-logo nav-logo">
-                <img src={logo} className="App-logo" alt="logo" />
-              </div>
+          <div className="app-body">
+            <div className="App-header">
+              <h2>Welcome to React</h2>
             </div>
-            <div className="nav-item">
-              <div className='label'>Daily</div>
-              <div className="App-logo nav-logo">
-                <img src={logo} className="App-logo" alt="logo" />
-              </div>
-            </div>
-            <div className="nav-item">
-              <div className='label'>Daily</div>
-              <div className="App-logo nav-logo">
-                <img src={logo} className="App-logo" alt="logo" />
-              </div>
-            </div>
-          </div>
-          <div className="toggle-collapse" onClick={() => this.setState({sideNavExpanded: !this.state.sideNavExpanded})}>
-            {this.state.sideNavExpanded &&  'c'}
-            {!this.state.sideNavExpanded &&  'e'}
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
           </div>
         </div>
-        <div className="app-body">
-          <div className="App-header">
-            <h2>Welcome to React</h2>
-          </div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
