7ff3b323b0d69deb89259f6fc0ef4a7aa853fad0
Doreen Hakimi <doreen.hakimi@gmail.com>
2017-07-22 17:33:18 -0700

    view hides/shows on toggle

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import ClassNames from 'classnames';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {ListItem} from 'material-ui/List';
+import RaisedButton from 'material-ui/RaisedButton';
+import Toggle from 'material-ui/Toggle';
+import { IconMenu, MenuItem } from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import { dropRight, pull } from 'lodash';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,7 +16,24 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {sideNavExpanded: true};
+    injectTapEventPlugin();
+    this.state = {
+      sideNavExpanded: true,
+      viewMode: 'view',
+      activeJournals: ['pjnl'],
+      journals: [
+        {
+          name: 'Personal Journal',
+          key: 'pjnl'
+        },
+        {
+          name: 'Business Journal',
+          key: 'bjnl'
+        }
+      ]
+    };
+    this.setViewMode = this.setViewMode.bind(this);
+    this.toggleActiveJournal = this.toggleActiveJournal.bind(this);
   }
 
   render() {
@@ -26,6 +50,7 @@ class App extends Component {
               </ListItem>
             </div>
             <div className={ClassNames({'side-nav-contents': true, expanded: this.state.sideNavExpanded})}>
+              {this.renderToggleView()}
               <ListItem className="nav-item">
                 <div className='label'>Daily</div>
                 <div className="App-logo nav-logo">
@@ -61,6 +86,48 @@ class App extends Component {
         </div>
       </MuiThemeProvider>
     );
+  }
+
+  renderToggleView () {
+    return (
+      <div className='toggle-view'>
+        <div className='header'>Mode</div>
+        <RaisedButton label='View' onClick={() => this.setViewMode('view')} primary={this.state.viewMode==='view'} />
+        <RaisedButton label='Plan' onClick={() => this.setViewMode('plan')} primary={this.state.viewMode==='plan'} />
+        <div className='journals'>
+          <div className='header'>My Journals</div>
+            {this.state.journals.map(({ name, key }) => (
+              <Toggle
+                key={key}
+                label={name}
+                toggled={this.state.activeJournals.indexOf(key) !== -1}
+                onToggle={() => this.toggleActiveJournal(key)}
+              />
+            ))}
+        </div>
+      </div>
+    );
+  }
+
+  setViewMode (viewMode) {
+    this.setState({viewMode});
+    if (viewMode==='plan' && this.state.activeJournals.length > 1) {
+      this.setState({activeJournals: dropRight(this.state.activeJournals)});
+    }
+  }
+
+  toggleActiveJournal (activeJournal) {
+    let newActiveJournals = this.state.activeJournals;
+    if (this.state.viewMode ==='view') {
+      if (newActiveJournals.indexOf(activeJournal) !== -1) {
+        newActiveJournals = pull(newActiveJournals, activeJournal);
+      } else {
+        newActiveJournals.push(activeJournal);
+      }
+      this.setState({activeJournals: newActiveJournals});
+    } else {
+      this.setState({activeJournals: [activeJournal]});
+    }
   }
 }
 
