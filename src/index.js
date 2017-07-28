e7c78679182641df7ecfcb915cdb1bf8b849df11
Doreen Hakimi <doreen.hakimi@gmail.com>
2017-07-27 20:47:34 -0700

    Squashed commit of the following:
    commit cc0a34ee3105ce6ec4d7b867bb63c523d27db01e
    Author: Doreen Hakimi <doreen.hakimi@gmail.com>
    Date:   Thu Jul 27 20:47:11 2017 -0700
    
        more fixes
    
    commit 2343e471695aea47f20e8f44fbbfb4018e6b35f3
    Author: Doreen Hakimi <doreen.hakimi@gmail.com>
    Date:   Thu Jul 27 20:41:44 2017 -0700
    
        finally geez

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './index.styl';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
